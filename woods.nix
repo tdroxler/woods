d549c2d00a6fd1066bac40c50d8d4d185854cd18
Florian Duraffourg <uni+git@duraffourg.eu>
2019-11-08 15:13:57 +0100

    Add material to build project on nix

---
+++ woods.nix
@@ -0,0 +1,31 @@
+{ mkDerivation, aeson, attoparsec, base, bytestring, Cabal
+, directory, filemanip, filepath, haskell-lsp-types, hspec
+, hspec-discover, microlens, network_3_1_1_0, proto-lens, proto-lens-protoc
+, proto-lens-runtime, proto-lens-setup, stdenv, text, pkgs
+}:
+mkDerivation {
+  pname = "woods";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  setupHaskellDepends = [ base Cabal proto-lens-setup ];
+  buildTools = [ pkgs.protobuf ];
+  libraryHaskellDepends = [
+    aeson attoparsec base bytestring directory filemanip filepath
+    haskell-lsp-types microlens network_3_1_1_0 proto-lens proto-lens-runtime
+    text 
+  ];
+  libraryToolDepends = [ proto-lens-protoc ];
+  executableHaskellDepends = [
+    aeson base bytestring haskell-lsp-types microlens network_3_1_1_0
+    proto-lens proto-lens-runtime text
+  ];
+  testHaskellDepends = [
+    base bytestring directory haskell-lsp-types hspec microlens
+    proto-lens text
+  ];
+  testToolDepends = [ hspec-discover ];
+  license = "unknown";
+  hydraPlatforms = stdenv.lib.platforms.none;
+}
