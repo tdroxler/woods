668dd0068ba3829e28b3de638fd231a3dcda8ba0
Thomas Droxler <thomas.droxler@gmail.com>
2020-07-06 07:33:04 +0200

    WIP

--- woods.nix
+++ woods.nix
@@ -1,20 +1,20 @@
 { mkDerivation, aeson, attoparsec, base, bytestring, Cabal
 , directory, filemanip, filepath, haskell-lsp-types, hspec
-, hspec-discover, microlens, network, proto-lens, proto-lens-protoc
-, proto-lens-runtime, proto-lens-setup, stdenv, text, pkgs
+, hspec-discover, microlens, network, process, proto-lens
+, proto-lens-protoc, proto-lens-runtime, proto-lens-setup, stdenv
+, text
 }:
 mkDerivation {
   pname = "woods";
   version = "0.1.0.0";
   src = ./.;
-  isLibrary = false;
+  isLibrary = true;
   isExecutable = true;
   setupHaskellDepends = [ base Cabal proto-lens-setup ];
-  buildTools = [ pkgs.protobuf ];
   libraryHaskellDepends = [
     aeson attoparsec base bytestring directory filemanip filepath
-    haskell-lsp-types microlens network proto-lens proto-lens-runtime
-    text
+    haskell-lsp-types microlens network process proto-lens
+    proto-lens-runtime text
   ];
   libraryToolDepends = [ proto-lens-protoc ];
   executableHaskellDepends = [
