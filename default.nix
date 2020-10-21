cdff8f637ebada95afc18052c1b754e4cab969bd
Thomas Droxler <thomas.droxler@gmail.com>
2020-10-21 07:57:21 +0200

    Use a fixed nixpkgs version in `default.nix`

--- default.nix
+++ default.nix
@@ -1,2 +1,13 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc865" }:
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./woods.nix { }
+{}:
+let
+  nixpkgs-20_03 = fetchTarball {
+    name   = "nixpkgs-stable-20_03";
+    url    = "https://github.com/NixOS/nixpkgs/archive/20.03.tar.gz";
+    sha256 = "0182ys095dfx02vl2a20j1hz92dx3mfgz2a6fhn31bqlp1wa8hlq";
+  };
+
+  nixpkgs = import nixpkgs-20_03 {};
+  haskellPackages = nixpkgs.pkgs.haskellPackages;
+in
+nixpkgs.pkgs.haskell.packages.ghc865.callPackage ./woods.nix { network = haskellPackages.network_3_1_1_1; haskell-lsp-types = haskellPackages.haskell-lsp-types_0_20_0_0;  }
+
